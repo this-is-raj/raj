3560909eb6d99b4f6a257fde960624c9b3e66247
Raj Kumar <rajkjjn@gmail.com>
2018-05-30 09:19:55 +0530

    Change: test with different webpack builiding

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../'),
     assetsSubDirectory: 'dist/static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
