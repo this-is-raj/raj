831a0c829572660126cd5fbc976fb0e7ab471632
Raj Kumar <rajkumar@froiden.com>
2018-06-03 12:19:03 +0530

    Change: try making dir raj

--- config/index.js
+++ config/index.js
@@ -42,7 +42,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../'),
-    assetsSubDirectory: 'dist/static',
+    assetsSubDirectory: 'raj/dist/static',
     assetsPublicPath: '/',
 
     /**
